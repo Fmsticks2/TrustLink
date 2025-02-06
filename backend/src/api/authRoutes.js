bb69254eb5c3a5128dcc3959b4020419b5eadc70
icekidtech <princeklvn7568@gmail.com>
2025-02-06 04:03:09 +0100

    added node server redefined imports for authRoutes.js Updated file name from BIdOverview.jsx to BidOverview.jsx

--- backend/src/api/authRoutes.js
+++ backend/src/api/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const pool = require("../config/db"); // Import PostgreSQL database connection
+const pool = require("../../config/db"); // Import PostgreSQL database connection
 require("dotenv").config();
 
 const router = express.Router();
